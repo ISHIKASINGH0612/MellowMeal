8367634cb8c47b445323b3055eef05bdc1789f1b
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2025-11-20 01:05:09 +0530

    creating card for dish

--- src/components/Basics/restaurant.jsx
+++ src/components/Basics/restaurant.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import "./style.css";
 
 const Restaurant = () => {
+
+  const myStyle = {
+    color: 'red'
+  };
+
   return (
     <>
       <div className="card-container">
         <div className="card">
           <div className="card-body">
             <span className="card-number card-circle subtle">1</span>
+            <span className="card-author subtle" style={myStyle}>Breakfast</span>
+            <h2 className='card-title'>Maggi</h2>
+            <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos totam officiis, dolorum sint recusandae dolor maiores ex nemo, libero nesciunt voluptatem, veritatis illum officia minus accusantium ab repudiandae magnam beatae?</span>
+            <div className='card-read'>Read</div>
           </div>
+          <img src="public\images\maggi.jpg" alt="images" className='card-media' />
+          <span className='card-tag subtle'>Order Now</span>
         </div>
       </div>
     </>
