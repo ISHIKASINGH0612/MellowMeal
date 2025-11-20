1c3c65c75e839e2d59cdbd48069a0dccb24418c8
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2025-11-20 11:02:06 +0530

    creating menuCard

--- src/components/Basics/restaurant.jsx
+++ src/components/Basics/restaurant.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import "./style.css";
-import menu from '../../menuApi';
+ import Menu from './menuApi';
 
 const Restaurant = () => {
 
-  const myStyle = {
-    color: 'red'
-  };
+  const [menuData, setMenuData] = React.useState(Menu);
+
 
   return (
     <>
@@ -14,12 +13,12 @@ const Restaurant = () => {
         <div className="card">
           <div className="card-body">
             <span className="card-number card-circle subtle">1</span>
-            <span className="card-author subtle" style={myStyle}>Breakfast</span>
-            <h2 className='card-title'>Maggi</h2>
+            <span className="card-author subtle" >Breakfast</span>
+            <h2 className='card-title' >Maggi</h2>
             <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos totam officiis, dolorum sint recusandae dolor maiores ex nemo, libero nesciunt voluptatem, veritatis illum officia minus accusantium ab repudiandae magnam beatae?</span>
             <div className='card-read'>Read</div>
           </div>
-          <img src="public\images\maggi.jpg" alt="images" className='card-media' />
+          {/* <img src="/images/maggi.jpg" alt="images" className='card-media' /> */}
           <span className='card-tag subtle'>Order Now</span>
         </div>
       </div>
